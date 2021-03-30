f485b17e5a9daf9d383dd16b37ee74687586eb6e
Jared Larter <49499860+Dr-Slugma@users.noreply.github.com>
2021-03-30 09:29:13 -0700

    Update test.m

--- test.m
+++ test.m
@@ -1,16 +1,19 @@
 anchor = imread('train01.tif');
 target = imread('train02.tif');
+%anchor = anchor(:,:,1);
+%target = target(:,:,1);
+block_size = 4;
 
 tic
-mvs = bma_tools.hbma(anchor, target, 3, 4, [1, 2, 3]);
+mvs = bma_tools.hbma(anchor, target, 3, block_size, [1, 2, 3]);
 toc
 
-% pimg = bma_tools.predict_image(target, mvs, 4);
+% pimg = bma_tools.predict_image(target, mvs, block_size);
 % bma_tools.plot_frames(pimg, anchor, mvs);
 
 tic
-mvs = bma_tools.hex_search(anchor, target, 4, 16);
+mvs = bma_tools.hex_search(anchor, target, block_size, 16);
 toc
 % 
-% pimg = bma_tools.predict_image(target, mvs, 4);
+% pimg = bma_tools.predict_image(target, mvs, block_size);
 % bma_tools.plot_frames(pimg, anchor, mvs);
